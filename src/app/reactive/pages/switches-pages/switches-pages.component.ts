6a87f25bb3ff201e51dbc343a6cc79a24d5ed80b
Ricardo Anchaluisa <ricardoanchaluisa@gmail.com>
2025-06-04 21:12:40 -0500

    Validaciones formulario switches y radio buttons

--- src/app/reactive/pages/switches-pages/switches-pages.component.ts
+++ src/app/reactive/pages/switches-pages/switches-pages.component.ts
@@ -1,9 +1,27 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-pages',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-pages.component.html',
 })
-export class SwitchesPagesComponent { }
+export class SwitchesPagesComponent {
+
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    gender: [null, Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
+  }
+
+}
